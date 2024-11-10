8d348432d0db22b874cc03b2ee38cd64a5da5292
hearn <aayushkaintura9481@gmail.com>
2024-11-10 13:46:23 +0530

    hrn

--- trees/DFS/q9.cc
+++ trees/DFS/q9.cc
@@ -1,31 +1,26 @@
 #include"includes.hpp"
 
-void dfs(TreeNode* root, int &sum,int currLeft)
+void dfs(TreeNode* root, int &sum,bool left)
 {
-
-    if(!root)
-    {
-        sum+=currLeft;
-        return ;
-    }
-    if(root->left)
-    {
-        currLeft=root->left->val;
-        dfs(root->left,sum,currLeft);
-    }
-    if(root->right) dfs(root->right,sum,currLeft);
+    if(!root) return;
+    //// we are setting the eft switches to be true 
+    dfs(root->left,sum,true);
+    dfs(root->right,sum,false);
+    //!! Main condition : if it is a leaf node and it is left leaf, then sum it baby
+    if(!root->left and !root->right and left) sum+= (root->val); 
 } 
+
 int sumOfLeftLeaves(TreeNode* root) {
     int sum=0;
-    dfs(root,sum,0);
+    dfs(root,sum,false);
     return sum;
 }
 
 int main()
 {
     
-    std::vector<int> a={1,2,3,4,5};
+    std::vector<int> a={1,2,3,4,5,4,3};
     TreeNode* node=makeTree(0,a.size()-1,a);
-    // std::cout<<node;
+    std::cout<<node<<"\n";
     std::cout<<sumOfLeftLeaves(node);
 }
